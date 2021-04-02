bccd0846f05df6c4add0bcc4b77f1fbbfca7c4a3
Rena Suzuki Wagner <rena.suzuki.wagner@gmail.com>
2021-04-02 16:22:21 +0200

    Added react-bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
-import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
