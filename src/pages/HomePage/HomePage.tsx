9338e38e2070b826f61481ce7a33ea034d4a4971
Rena Suzuki Wagner <rena.suzuki.wagner@gmail.com>
2021-04-15 13:51:42 +0200

    Fixed HomePage space

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -9,49 +9,48 @@ import StyleImage from "../../assets/StyleImage.jpeg";
 export default function HomePage() {
   const isLoading = useSelector(selectLoading);
   return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: 900 }}
+    >
       <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: 900 }}
+        className="p-5 text-center bg-image"
+        style={{
+          backgroundImage: `url(${StyleImage})`,
+          width: "100%",
+          height: 900,
+        }}
       >
-        <div
-          className="p-5 text-center bg-image"
-          style={{
-            backgroundImage: `url(${StyleImage})`,
-            width: "100%",
-            height: 900,
-          }}
-        >
-          <div className="d-flex justify-content-center align-items-center h-100">
-            <div
-              className="mask p-5"
-              style={{ backgroundColor: `rgba(211,211,211, 0.6)` }}
-            >
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <div
+            className="mask p-5"
+            style={{ backgroundColor: `rgba(211,211,211, 0.6)` }}
+          >
+            <div className="text-white">
+              <h1
+                className="font-weight-bold pl-3 pr-3"
+                style={{ fontSize: 80 }}
+              >
+                Comfort Outfit
+              </h1>
+              <h2 className="mt-3 mb-5 pl-3 pr-3">
+                Find the perfet outfit that matches the weather and temperature
+              </h2>
               <div className="text-white">
-                <h1
-                  className="font-weight-bold pl-3 pr-3"
-                  style={{ fontSize: 80 }}
-                >
-                  Comfort Outfit
-                </h1>
-                <h2 className="mt-3 mb-5 pl-3 pr-3">
-                  Find the perfet outfit that matches the weather and
-                  temperature
-                </h2>
-                <div className="text-white">
-                  <LocationButton type="style" />
-                  <LocationInput type="style" />
-                </div>
-                {isLoading ? (
-                  <Spinner animation="border" role="status" className="mt-5">
-                    <span className="sr-only">Loading...</span>
-                  </Spinner>
-                ) : (
-                  <></>
-                )}
+                <LocationButton type="style" />
+                <LocationInput type="style" />
               </div>
+              {isLoading ? (
+                <Spinner animation="border" role="status" className="mt-5">
+                  <span className="sr-only">Loading...</span>
+                </Spinner>
+              ) : (
+                <></>
+              )}
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 }
